0e50946417d64a25b1e9470c1cedf79b53737bb8
Caique Silveira <38363493+CaiqueSilveira@users.noreply.github.com>
2021-03-18 11:06:52 -0300

    Ajustes na Feature de Edição

--- features/specs/editar.feature
+++ features/specs/editar.feature
@@ -7,8 +7,8 @@ Funcionalidade: Editar
     Cenario: Editar Vaga Ativa
         Dado que acesso a página inicial
             E clico no botão Editar
-            E altero campo Título
-            E altero o campo Descrição
+            E altero campo Título da vaga ativa
+            E altero o campo Descrição da vaga ativa
             E altero o Status para Inativo
             E clico no botão Enviar
         Então sou redirecionado para tela inicial com a mensagem de sucesso: "Ação executada com sucesso!"
@@ -16,8 +16,8 @@ Funcionalidade: Editar
     Cenario: Editar Vaga Inativa
         Dado que acesso a página inicial
             E clico no botão Editar
-            E altero campo Título
-            E altero o campo Descrição
+            E altero campo Título da vaga inativa
+            E altero o campo Descrição da vaga inativa
             E altero o Status para Ativo
             E clico no botão Enviar
         Então sou redirecionado para tela inicial com a mensagem de sucesso: "Ação executada com sucesso!"
--- features/step_definitions/editar.rb
+++ features/step_definitions/editar.rb
@@ -1,30 +1,37 @@
 #Editar Vaga Ativa
 Dado("clico no botão Editar") do
-  find("body > div > main > section:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(6) > a:nth-child(1) > button", visible: false).set "Teste Automatizado Editado"
+  sleep 3
+  find("body > div > main > section:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(6) > a:nth-child(1) > button").click
 end
 
-Dado("altero campo Título") do
+Dado("altero campo Título da vaga ativa") do
+  sleep 3
   find("body > div > main > form > div:nth-child(1) > input").set "Teste Automatizado Editado Inativo"
 end
 
-Dado("altero o campo Descrição") do
+Dado("altero o campo Descrição da vaga ativa") do
+  sleep 3
   find("body > div > main > form > div:nth-child(2) > textarea").set "Teste Automatizado Editado"
 end
 
 Dado("altero o Status para Inativo") do
+  sleep 3
   find("body > div > main > form > div:nth-child(3) > div > div:nth-child(2) > label").click
 end
 
 #Editar Vaga Inativa
 
-Dado("altero campo Título") do
+Dado("altero campo Título da vaga inativa") do
+  sleep 3
   find("body > div > main > form > div:nth-child(1) > input").set "Teste Automatizado Editado Ativo"
 end
 
-Dado("altero o campo Descrição") do
+Dado("altero o campo Descrição da vaga inativa") do
+  sleep 3
   find("body > div > main > form > div:nth-child(2) > textarea").set "Teste Automatizado Editado"
 end
 
 Dado("altero o Status para Ativo") do
+  sleep 3
   find("body > div > main > form > div:nth-child(3) > div > div:nth-child(1) > label").click
 end
